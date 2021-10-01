c4a96c4fe15c747d433b4f2b151f952015743354
Mr-Kon <yashkon@live.com>
2021-09-30 17:30:54 -0700

    ay1

--- AY1.py
+++ AY1.py
@@ -10,11 +10,11 @@ pings = input('How many pings do you want to send: ')
 i = 0
 while i < int(pings):
 	clientSocket.settimeout(1)
+	message = f'seq {i+1} {time.asctime()},'
 	startTime = time.time()*1000
-	clientSocket.sendto(pings.encode(), (serverName, clientPort))
+	clientSocket.sendto(message.encode(), (serverName, clientPort))
 	modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 	pingTimes.append(round(time.time()*1000 - startTime, 2))
-	print(type(serverAddress))
 	print(f'Ping {i+1}: host {serverAddress[0]} replied: {modifiedMessage.decode()} RTT = {pingTimes[i]} ms')
 	#print(modifiedMessage.decode())
 	i += 1
@@ -26,10 +26,6 @@ maxTime = pingTimes[len(pingTimes)-1]
 minTime = pingTimes[0]
 for j in pingTimes:
 	avgTime += j
-#	if maxTime > j:
-#		maxTime = j
-#	if minTime < j:
-#		minTime = j
 avgTime = avgTime/float(pings)
 print('Minimum RTT:', maxTime, 'ms')
 print('Maximum RTT:', minTime, 'ms')
